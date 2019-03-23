99261e418bcf0baa3c0708dec2c8fbe2861789e5
jasonmarcello <jkm7264@g.rit.edu>
2019-03-22 21:16:37 -0400

    act3 finally done

---
+++ hw5/act3/app/armbook/logout.php
@@ -0,0 +1,20 @@
+<?php
+// Unset all of the session variables.
+//$_SESSION = array();
+
+// If it's desired to kill the session, also delete the session cookie.
+// Note: This will destroy the session, and not just the session data!
+//if (ini_get("session.use_cookies")) {
+   /// $params = session_get_cookie_params();
+   // setcookie(session_name(), '', time() - 42000,
+  //      $params["path"], $params["domain"],
+ //       $params["secure"], $params["httponly"]
+//    );
+//}
+//session_unset(); 
+// Finally, destroy the session.
+session_start();
+session_destroy();
+
+echo "<script>window.location.href = '/index.php';</script>";
+?>
